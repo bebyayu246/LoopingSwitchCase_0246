a6f00b69333daa5d6176fbdc70a600c430014a35
bebyayu246 <beby.ayu.ft24@mail.umy.ac.id>
2025-03-17 19:47:32 +0700

    library

---
+++ tempCodeRunnerFile.cpp
@@ -0,0 +1,5 @@
+ for(counter = 0; counter < 5; counter++){
+        cout <<"Nilai counter = " << counter << " TI UMY" << endl;
+    }
+    cout << " Nilai counter terakhir = " << counter << endl;
+    cout << endl;
